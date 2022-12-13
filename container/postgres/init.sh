f21b481ada36a28b672f002de4f3cdc950daa54f
Jorge Dieguez Pérez <jdieguezbean@gmail.com>
2022-12-13 16:50:57 +0200

    Update init.sh

--- container/postgres/init.sh
+++ container/postgres/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Init database with user $POSTGRES_USER and  create database $POSTGRES_DB"
-psql -U $POSTGRES_USER CREATE DATABASE $POSTGRES_DB;
+psql --username $POSTGRES_USER CREATE DATABASE $POSTGRES_DB;
