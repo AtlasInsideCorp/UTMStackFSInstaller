48b4c35e925490794324cddbc01e47692d8727b6
Jorge Dieguez Pérez <jdieguezbean@gmail.com>
2022-12-13 12:51:56 +0200

    Set image tags

--- bin/fs_images.sh
+++ bin/fs_images.sh
@@ -2,7 +2,8 @@
 set -e
 sudo docker login -u client -p 4xYkVIAH8kdAH7mP/9BBhbb2ByzLGm4F utmstack.azurecr.io
 
-docker pull utmstack.azurecr.io/utmstack_federation_service_backend:${TAG}
-docker pull utmstack.azurecr.io/utmstack_federation_service_frontend:${TAG}
-docker pull utmstack.azurecr.io/postgres:${TAG}
+
+docker pull utmstack.azurecr.io/utmstack_federation_service_backend:v9
+docker pull utmstack.azurecr.io/utmstack_federation_service_frontend:v9
+docker pull utmstack.azurecr.io/postgres:v9
 docker pull containrrr/watchtower:latest
