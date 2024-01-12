cb64604825b9b98408968e05fae93fec425592c2
Carlos Guzman <63058247+karlbrown87@users.noreply.github.com>
2024-01-12 16:51:05 +0200

    Update fs_images.sh

--- bin/fs_images.sh
+++ bin/fs_images.sh
@@ -3,7 +3,7 @@ set -e
 sudo docker login -u client -p 4xYkVIAH8kdAH7mP/9BBhbb2ByzLGm4F utmstack.azurecr.io
 
 
-docker pull utmstack.azurecr.io/utmstack_federation_service_backend:v9
-docker pull utmstack.azurecr.io/utmstack_federation_service_frontend:v9
-docker pull utmstack.azurecr.io/postgres:v9
+docker pull utmstack.azurecr.io/utmstack_federation_service_backend:10.0.0
+docker pull utmstack.azurecr.io/utmstack_federation_service_frontend:10.0.0
+docker pull utmstack.azurecr.io/utmstack_federation_service_postgres:10.0.0
 docker pull containrrr/watchtower:latest
